099ffd24e4d6b236330add024aea2e499c00b8a7
Priya Mehta <priya.mehta@green-apex.com>
2018-03-28 17:45:20 +0530

    yml changed

--- scripts/app_deployment.sh
+++ scripts/app_deployment.sh
@@ -1,5 +1,5 @@
 echo "Deployment script by Priya"
-cp  -Rv /var/TMWithSpring/index.html /var/lib/tomcat8/webapps/ROOT
+cp  -Rv /var/TaskManagementWithSpringFrontend/index.html /var/lib/tomcat8/webapps/ROOT
 sleep 10
 
 
